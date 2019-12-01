70b76adc596e8b117ef5aea9dc0a479846a6f70e
victor <jvmvik>
2019-11-30 20:45:39 -0600

    improve test by running a sequence

--- ngpio_test.go
+++ ngpio_test.go
@@ -1,22 +1,40 @@
 package ngpio
 
 import (
+	"sync"
 	"testing"
 	"time"
 )
 
 func TestMain(m *testing.M) {
+	var wg sync.WaitGroup
 	spec := Nano()
-	port, err := spec.FindPort(12)
-	if err != nil {
-		panic(err)
+	list := []int{7, 11, 13, 15}
+
+	var err error
+	var port Port
+
+	for _, i := range list {
+		wg.Add(1)
+		func(i int) {
+
+			port, err = spec.FindPort(i)
+			if err != nil {
+				panic(err)
+			}
+
+			delay := time.Second * 1
+			port.Output()
+			port.High()
+			time.Sleep(delay)
+			port.Low()
+			time.Sleep(delay)
+			port.High()
+
+			defer wg.Done()
+		}(i)
+
 	}
-	port.Output()
-	port.High()
-	time.Sleep(time.Second * 1)
-	port.Low()
-	time.Sleep(time.Second * 1)
-	port.High()
-	time.Sleep(time.Second * 1)
-	port.Low()
+
+	wg.Wait()
 }
